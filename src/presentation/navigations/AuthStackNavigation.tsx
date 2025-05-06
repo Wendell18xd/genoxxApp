ed0e2af4242165b5ebb70a3d017a844798f05f83
Wendell Smith Vidal <wendell18cuenta@gmail.com>
2025-05-06 09:50:14 -0500

    Merge pull request #4 from Wendell18xd:create/login-screen
    Create/login-screen

--- src/presentation/navigations/AuthStackNavigation.tsx
+++ src/presentation/navigations/AuthStackNavigation.tsx
@@ -1,14 +1,16 @@
-import {createStackNavigator} from '@react-navigation/stack';
+import {createStackNavigator, TransitionPresets} from '@react-navigation/stack';
 import LoginScreen from '../features/auth/screens/LoginScreen';
 import OlvidarPassScreen from '../features/auth/screens/OlvidarPassScreen';
 import GetStartScreen from '../features/auth/screens/GetStartScreen';
 import CambioPassScreen from '../features/auth/screens/CambioPassScreen';
+import HomeScreen from '../features/main/screens/HomeScreen';
 
 export type AuthStackParam = {
   GetStartScreen: undefined;
   LoginScreen: undefined;
   OlvidarPassScreen: undefined;
   CambioPassScreen: undefined;
+  HomeScreen: undefined;
 };
 
 const Stack = createStackNavigator<AuthStackParam>();
@@ -22,6 +24,7 @@ export const AuthStackNavigation = () => {
       <Stack.Screen name="LoginScreen" component={LoginScreen} />
       <Stack.Screen name="OlvidarPassScreen" component={OlvidarPassScreen} />
       <Stack.Screen name="CambioPassScreen" component={CambioPassScreen} />
+      <Stack.Screen name="HomeScreen" component={HomeScreen} />
     </Stack.Navigator>
   );
 };
