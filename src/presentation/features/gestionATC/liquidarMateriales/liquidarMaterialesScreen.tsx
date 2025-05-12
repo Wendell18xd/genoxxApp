1f03a16cf818d011de43c6d138762e13bede3927
Francesca <francescadelzo14@gmail.com>
2025-05-12 10:57:27 -0500

    feat: Add liquidarMaterialesScreen and update AuthStackNavigation

---
+++ src/presentation/features/gestionATC/liquidarMateriales/liquidarMaterialesScreen.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text } from 'react-native-gesture-handler';
+
+export const liquidarMaterialesScreen = () => {
+  return (
+    <Text>liquidarMaterialesScreen</Text>
+  );
+};
