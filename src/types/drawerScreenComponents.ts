e79c5c2db9252cee75874ee86d5e35f418c7a07e
Wendell Smith <wendell18cuenta@gmail.com>
2025-05-12 16:06:56 -0500

    fix: Agregar ListMaterialesATCFragment al registro de componentes del menú lateral

--- src/types/drawerScreenComponents.ts
+++ src/types/drawerScreenComponents.ts
@@ -10,4 +10,5 @@ export const drawerScreenComponents: Record<
   PedidoMaterialFragment: LiquidarPartidasObras,
   AprobacionPedidosFragment: LiquidarPartidasObras,
   ReporteStockFragment: LiquidarPartidasObras,
+  ListMaterialesATCFragment: LiquidarPartidasObras,
 };
