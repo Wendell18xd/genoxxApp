c56e8a571c159a19d89686f94024a2f1dacf3f98
Erick Pereyra <erickgames131@gmail.com>
2025-05-19 13:11:03 -0500

    feat: Implement HeaderDetalle and ListaLiquidados components for enhanced material details display

--- src/presentation/features/gestionATC/LiquidarMateriales/screens/DetalleLiquidarMatScreen.tsx
+++ src/presentation/features/gestionATC/LiquidarMateriales/screens/DetalleLiquidarMatScreen.tsx
@@ -1,8 +1,35 @@
-import React, {useRef} from 'react';
-import {Animated, StyleSheet, View} from 'react-native';
-import {Card, Divider, Text} from 'react-native-paper';
+import React, { useRef } from 'react';
+import { Animated, StyleSheet, View } from 'react-native';
 import DrawerLayout from '../../../main/layout/DrawerLayout';
-import MaterialIcons from '../../../../components/ui/icons/MaterialIcons';
+import HeaderDetalle from '../components/HeaderDetalle';
+import ListaLiquidados from '../components/ListaLiquidados';
+
+const materialesMock = [
+  {
+    id: '1',
+    codMaterial: 'CABLE 1X4',
+    nombre: 'Cable de cobre',
+    sku: 'MTR',
+    cantidad: '100',
+    motivo: 'Instalación',
+  },
+  {
+    id: '2',
+    codMaterial: 'CONECTOR F',
+    nombre: 'Conector rápido',
+    sku: 'PZA',
+    cantidad: '10',
+    motivo: 'Reemplazo',
+  },
+  {
+    id: '3',
+    codMaterial: 'CAJA NAP',
+    nombre: 'Caja de distribución',
+    sku: 'UND',
+    cantidad: '1',
+    motivo: 'Nueva instalación',
+  },
+];
 
 const DetalleLiquidarMatScreen = () => {
   const scrollY = useRef(new Animated.Value(0)).current;
@@ -25,346 +52,15 @@ const DetalleLiquidarMatScreen = () => {
   return (
     <DrawerLayout primary curvaHeight={80}>
       <View style={styles.wrapper}>
-        {/* Card principal siempre visible */}
-        <Card style={[styles.card, styles.cardMain]}>
-          <Card.Content>
-            <View style={styles.listContainer}>
-              {/* Campos SIEMPRE visibles */}
-              <View style={styles.row}>
-                <MaterialIcons
-                  name="folder-text-outline"
-                  style={{marginRight: 6}}
-                />
-                <View style={styles.titleContainer}>
-                  <Text style={styles.title}>Proyecto:</Text>
-                </View>
-                <View style={styles.descriptionContainer}>
-                  <Text style={styles.description}>IVTRC</Text>
-                </View>
-              </View>
-              <View style={styles.row}>
-                <MaterialIcons
-                  name="file-document-outline"
-                  style={{marginRight: 6}}
-                />
-                <View style={styles.titleContainer}>
-                  <Text style={styles.title}>Unidad de Negocio:</Text>
-                </View>
-                <View style={styles.descriptionContainer}>
-                  <Text style={styles.description}>UN09</Text>
-                </View>
-              </View>
-              <View style={styles.row}>
-                <MaterialIcons
-                  name="format-list-numbered"
-                  style={{marginRight: 6}}
-                />
-                <View style={styles.titleContainer}>
-                  <Text style={styles.title}>Nro Petición:</Text>
-                </View>
-                <View style={styles.descriptionContainer}>
-                  <Text style={styles.description}>1-32EAAZJ4</Text>
-                </View>
-              </View>
-              {/* Campos que se OCULTAN al hacer scroll */}
-              <Animated.View
-                style={{
-                  opacity: extraFieldsOpacity,
-                  height: extraFieldsHeight,
-                  overflow: 'hidden',
-                }}>
-                <View style={styles.row}>
-                  <MaterialIcons
-                    name="calendar-month"
-                    style={{marginRight: 6}}
-                  />
-                  <View style={styles.titleContainer}>
-                    <Text style={styles.title}>Fecha Liquidación:</Text>
-                  </View>
-                  <View style={styles.descriptionContainer}>
-                    <Text style={styles.description}>22/07/2024</Text>
-                  </View>
-                </View>
-
-                <View style={styles.row}>
-                  <MaterialIcons
-                    name="clock-time-four-outline"
-                    style={{marginRight: 6}}
-                  />
-                  <View style={styles.titleContainer}>
-                    <Text style={styles.title}>Hora Liquidación:</Text>
-                  </View>
-                  <View style={styles.descriptionContainer}>
-                    <Text style={styles.description}>00:00</Text>
-                  </View>
-                </View>
-                <View style={styles.row}>
-                  <MaterialIcons
-                    name="calendar-check"
-                    style={{marginRight: 6}}
-                  />
-                  <View style={styles.titleContainer}>
-                    <Text style={styles.title}>Fecha Emisión:</Text>
-                  </View>
-                  <View style={styles.descriptionContainer}>
-                    <Text style={styles.description}>01/01/1900</Text>
-                  </View>
-                </View>
-                <View style={styles.row}>
-                  <MaterialIcons
-                    name="clipboard-text"
-                    style={{marginRight: 6}}
-                  />
-                  <View style={styles.titleContainer}>
-                    <Text style={styles.title}>Tipo de Orden:</Text>
-                  </View>
-                  <View style={styles.descriptionContainer}>
-                    <Text style={styles.description}>1 PLAY SIN SERVICIO</Text>
-                  </View>
-                </View>
-                <View style={styles.row}>
-                  <MaterialIcons name="account" style={{marginRight: 6}} />
-                  <View style={styles.titleContainer}>
-                    <Text style={styles.title}>Nombre del Cliente:</Text>
-                  </View>
-                  <View style={styles.descriptionContainer}>
-                    <Text style={styles.description}>
-                      ALEJANDRA VENEGAS DONAIRE
-                    </Text>
-                  </View>
-                </View>
-                <View style={styles.row}>
-                  <MaterialIcons name="phone" style={{marginRight: 6}} />
-                  <View style={styles.titleContainer}>
-                    <Text style={styles.title}>Teléfono:</Text>
-                  </View>
-                  <View style={styles.descriptionContainer}>
-                    <Text style={styles.description}>123456789</Text>
-                  </View>
-                </View>
-              </Animated.View>
-            </View>
-
-            <Divider style={{marginVertical: 8}} />
-
-            <Text style={styles.titleMedium}>Estados</Text>
-            <View style={styles.statusContainer}>
-              <View style={[styles.statusBox, {backgroundColor: '#E8F5E9'}]}>
-                <Text style={styles.statusTitle}>Acta:</Text>
-                <Text
-                  style={[
-                    styles.statusValue,
-                    {color: getTextColor('#E8F5E9')},
-                  ]}>
-                  ABIERTA
-                </Text>
-              </View>
-              <View style={[styles.statusBox, {backgroundColor: '#E8F5E9'}]}>
-                <Text style={styles.statusTitle}>Orden:</Text>
-                <Text
-                  style={[
-                    styles.statusValue,
-                    {color: getTextColor('#E8F5E9')},
-                  ]}>
-                  ABIERTA
-                </Text>
-              </View>
-              <View style={[styles.statusBox, {backgroundColor: '#F5F5F5'}]}>
-                <Text style={styles.statusTitle}>Materiales:</Text>
-                <Text
-                  style={[
-                    styles.statusValue,
-                    {color: getTextColor('#F5F5F5')},
-                  ]}>
-                  PENDIENTE
-                </Text>
-              </View>
-            </View>
-          </Card.Content>
-        </Card>
-        <Text style={styles.externalTitle}>Materiales Liquidados</Text>
-
-        {/* Scroll solo para los materiales */}
-        <Animated.ScrollView
-          contentContainerStyle={styles.scrollContent}
-          showsVerticalScrollIndicator={false}
-          scrollEventThrottle={16}
-          onScroll={Animated.event(
-            [{nativeEvent: {contentOffset: {y: scrollY}}}],
-            {useNativeDriver: false},
-          )}>
-          <Card style={styles.card}>
-            <Card.Content>
-              <View style={styles.listContainer}>
-                <View style={styles.row}>
-                  <MaterialIcons
-                    name="package-variant-closed"
-                    style={{marginRight: 6}}
-                  />
-                  <View style={styles.titleContainer}>
-                    <Text style={styles.title}>Cod Material:</Text>
-                  </View>
-                  <View style={styles.descriptionContainer}>
-                    <Text style={styles.description}>CABLE 1X4</Text>
-                  </View>
-                </View>
-                <View style={styles.row}>
-                  <MaterialIcons name="cube" style={{marginRight: 6}} />
-                  <View style={styles.titleContainer}>
-                    <Text style={styles.title}>Nombre del material:</Text>
-                  </View>
-                  <View style={styles.descriptionContainer}>
-                    <Text style={styles.description}>100</Text>
-                  </View>
-                </View>
-                <View style={styles.row}>
-                  <MaterialIcons name="tag" style={{marginRight: 6}} />
-                  <View style={styles.titleContainer}>
-                    <Text style={styles.title}>Sku Cliente:</Text>
-                  </View>
-                  <View style={styles.descriptionContainer}>
-                    <Text style={styles.description}>MTR</Text>
-                  </View>
-                </View>
-                <View style={styles.row}>
-                  <MaterialIcons
-                    name="package-variant"
-                    style={{marginRight: 6}}
-                  />
-                  <View style={styles.titleContainer}>
-                    <Text style={styles.title}>Cantidad:</Text>
-                  </View>
-                  <View style={styles.descriptionContainer}>
-                    <Text style={styles.description}>MTR</Text>
-                  </View>
-                </View>
-                <View style={styles.row}>
-                  <MaterialIcons name="comment-text" style={{marginRight: 6}} />
-                  <View style={styles.titleContainer}>
-                    <Text style={styles.title}>Motivo:</Text>
-                  </View>
-                  <View style={styles.descriptionContainer}>
-                    <Text style={styles.description}>MTR</Text>
-                  </View>
-                </View>
-              </View>
-            </Card.Content>
-          </Card>
-          <Card style={styles.card}>
-            <Card.Content>
-              <View style={styles.listContainer}>
-                <View style={styles.row}>
-                  <MaterialIcons
-                    name="package-variant-closed"
-                    style={{marginRight: 6}}
-                  />
-                  <View style={styles.titleContainer}>
-                    <Text style={styles.title}>Cod Material:</Text>
-                  </View>
-                  <View style={styles.descriptionContainer}>
-                    <Text style={styles.description}>CABLE 1X4</Text>
-                  </View>
-                </View>
-                <View style={styles.row}>
-                  <MaterialIcons name="cube" style={{marginRight: 6}} />
-                  <View style={styles.titleContainer}>
-                    <Text style={styles.title}>Nombre del material:</Text>
-                  </View>
-                  <View style={styles.descriptionContainer}>
-                    <Text style={styles.description}>100</Text>
-                  </View>
-                </View>
-                <View style={styles.row}>
-                  <MaterialIcons name="tag" style={{marginRight: 6}} />
-                  <View style={styles.titleContainer}>
-                    <Text style={styles.title}>Sku Cliente:</Text>
-                  </View>
-                  <View style={styles.descriptionContainer}>
-                    <Text style={styles.description}>MTR</Text>
-                  </View>
-                </View>
-                <View style={styles.row}>
-                  <MaterialIcons
-                    name="package-variant"
-                    style={{marginRight: 6}}
-                  />
-                  <View style={styles.titleContainer}>
-                    <Text style={styles.title}>Cantidad:</Text>
-                  </View>
-                  <View style={styles.descriptionContainer}>
-                    <Text style={styles.description}>MTR</Text>
-                  </View>
-                </View>
-                <View style={styles.row}>
-                  <MaterialIcons name="comment-text" style={{marginRight: 6}} />
-                  <View style={styles.titleContainer}>
-                    <Text style={styles.title}>Motivo:</Text>
-                  </View>
-                  <View style={styles.descriptionContainer}>
-                    <Text style={styles.description}>MTR</Text>
-                  </View>
-                </View>
-              </View>
-            </Card.Content>
-          </Card>
-          <Card style={styles.card}>
-            <Card.Content>
-              <View style={styles.listContainer}>
-                <View style={styles.row}>
-                  <MaterialIcons
-                    name="package-variant-closed"
-                    style={{marginRight: 6}}
-                  />
-                  <View style={styles.titleContainer}>
-                    <Text style={styles.title}>Cod Material:</Text>
-                  </View>
-                  <View style={styles.descriptionContainer}>
-                    <Text style={styles.description}>CABLE 1X4</Text>
-                  </View>
-                </View>
-                <View style={styles.row}>
-                  <MaterialIcons name="cube" style={{marginRight: 6}} />
-                  <View style={styles.titleContainer}>
-                    <Text style={styles.title}>Nombre del material:</Text>
-                  </View>
-                  <View style={styles.descriptionContainer}>
-                    <Text style={styles.description}>100</Text>
-                  </View>
-                </View>
-                <View style={styles.row}>
-                  <MaterialIcons name="tag" style={{marginRight: 6}} />
-                  <View style={styles.titleContainer}>
-                    <Text style={styles.title}>Sku Cliente:</Text>
-                  </View>
-                  <View style={styles.descriptionContainer}>
-                    <Text style={styles.description}>MTR</Text>
-                  </View>
-                </View>
-                <View style={styles.row}>
-                  <MaterialIcons
-                    name="package-variant"
-                    style={{marginRight: 6}}
-                  />
-                  <View style={styles.titleContainer}>
-                    <Text style={styles.title}>Cantidad:</Text>
-                  </View>
-                  <View style={styles.descriptionContainer}>
-                    <Text style={styles.description}>MTR</Text>
-                  </View>
-                </View>
-                <View style={styles.row}>
-                  <MaterialIcons name="comment-text" style={{marginRight: 6}} />
-                  <View style={styles.titleContainer}>
-                    <Text style={styles.title}>Motivo:</Text>
-                  </View>
-                  <View style={styles.descriptionContainer}>
-                    <Text style={styles.description}>MTR</Text>
-                  </View>
-                </View>
-              </View>
-            </Card.Content>
-          </Card>
-        </Animated.ScrollView>
+        <HeaderDetalle
+          extraFieldsOpacity={extraFieldsOpacity}
+          extraFieldsHeight={extraFieldsHeight}
+          getTextColor={getTextColor}
+        />
+        <ListaLiquidados
+          data={materialesMock}
+          scrollY={scrollY}
+        />
       </View>
     </DrawerLayout>
   );
@@ -373,83 +69,6 @@ const DetalleLiquidarMatScreen = () => {
 const styles = StyleSheet.create({
   wrapper: {
     flex: 1,
-  },
-  scrollContent: {
-    padding: 16,
-  },
-  hiddenScroll: {
-    flex: 1,
-  },
-  card: {
-    borderRadius: 12,
-    elevation: 2,
-    marginBottom: 20,
-  },
-  cardMain: {
-    marginHorizontal: 16,
-    marginTop: 16,
-  },
-  listContainer: {
-    marginBottom: 10,
-  },
-  row: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginVertical: 4,
-    flexWrap: 'wrap',
-    gap: 4, // puedes ajustar esto según lo necesites
-  },
-  nameRow: {
-    marginBottom: 12,
-  },
-  title: {
-    fontWeight: 'bold',
-    fontSize: 16,
-  },
-  description: {
-    fontSize: 16,
-    textAlign: 'left',
-  },
-  titleMedium: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 8,
-  },
-  externalTitle: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    marginBottom: 8,
-    marginHorizontal: 16,
-  },
-  statusContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginTop: 10,
-  },
-  statusBox: {
-    width: 100,
-    height: 50,
-    padding: 8,
-    marginHorizontal: 4,
-    borderRadius: 8,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  statusTitle: {
-    fontSize: 12,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-  statusValue: {
-    fontSize: 14,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-  titleContainer: {
-    width: 170,
-  },
-  descriptionContainer: {
-    flex: 2,
   },
 });
 
