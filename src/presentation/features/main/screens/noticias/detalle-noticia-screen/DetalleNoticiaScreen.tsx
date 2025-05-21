bd1a770b99b90a55004df291e6229ab56f438cf1
Wendell Smith <wendell18cuenta@gmail.com>
2025-05-21 18:19:32 -0500

    feat: Refactor useDetalleNoticia para manejar la carga y mutación de archivos de noticias, mejorando la gestión del estado y la navegación

--- src/presentation/features/main/screens/noticias/detalle-noticia-screen/DetalleNoticiaScreen.tsx
+++ src/presentation/features/main/screens/noticias/detalle-noticia-screen/DetalleNoticiaScreen.tsx
@@ -1,63 +1,22 @@
 import SafeAreaLayout from '../../../layout/SafeAreaLayout';
-import {RouteProp, useNavigation, useRoute} from '@react-navigation/native';
+import {RouteProp, useRoute} from '@react-navigation/native';
 import {NoticiasStackParam} from '../navigations/NoticiasStackNatigation';
-import {useTheme} from 'react-native-paper';
-import {useMutation, useQuery} from '@tanstack/react-query';
-import {listadoArchivosNoticia} from '../../../../../../actions/main/main';
-import {useAuthStore} from '../../../../../store/auth/useAuthStore';
-import {RegistroDesdeNoticia} from '../../../../../../domain/entities/Noticia';
 import FullScreenLoader from '../../../../../components/ui/loaders/FullScreenLoader';
 import {CustomFAB} from '../../../../../components/ui/CustomFAB';
 import {FlatList, View} from 'react-native';
 import {DetalleHeaderNoticias} from './components/DetalleHeaderNoticias';
 import {ItemArchivoNoticia} from './components/ItemArchivoNoticia';
-import Toast from 'react-native-toast-message';
+import {useDetalleNoticia} from './hooks/useDetalleNoticia';
+import {useTheme} from 'react-native-paper';
 
 export const DetalleNoticiaScreen = () => {
   const {noticia, onLeido} =
     useRoute<RouteProp<NoticiasStackParam, 'DetalleNoticiaScreen'>>().params;
-  const {user} = useAuthStore();
   const {colors} = useTheme();
-  const navigation = useNavigation();
-
-  const {data: detalle, isLoading} = useQuery({
-    queryKey: ['archivosNoticia', noticia.cont_correlativo],
-    queryFn: () =>
-      listadoArchivosNoticia({
-        vl_empr_codigo: user?.empr_codigo || '',
-        vl_trab_codigo: user?.usua_perfil || '',
-        vl_cont_correlativo: noticia.cont_correlativo,
-        vl_is_visto: noticia.is_visto,
-        vl_registro_desde: RegistroDesdeNoticia.App,
-      }),
+  const {detalle, isLoading, handleMarcarLeido, mutation} = useDetalleNoticia({
+    noticia,
+    onLeido,
   });
-
-  const mutation = useMutation({
-    mutationFn: listadoArchivosNoticia,
-    onSuccess: data => {
-      console.log(data);
-      Toast.show({
-        type: 'success',
-        text1: 'Noticia marcada como leída',
-        text2: 'La noticia se ha marcado como leída correctamente',
-      });
-      onLeido();
-      navigation.goBack();
-    },
-    onError: error => {
-      Toast.show({type: 'error', text1: error.message});
-    },
-  });
-
-  const handleMarcarLeido = () => {
-    mutation.mutate({
-      vl_empr_codigo: user?.empr_codigo || '',
-      vl_trab_codigo: user?.usua_perfil || '',
-      vl_cont_correlativo: noticia.cont_correlativo,
-      vl_is_visto: 1,
-      vl_registro_desde: RegistroDesdeNoticia.App,
-    });
-  };
 
   return (
     <SafeAreaLayout title="Detalle de Noticia" isHeader primary>
