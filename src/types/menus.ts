23e361533991e66100f01d748ddc40b9dc5fed26
Wendell Smith <wendell18cuenta@gmail.com>
2025-05-28 13:07:59 -0500

    feat: Añadir nuevo valor 'CONSULTA_UNIDADES_FLOTA' al enum Menu para la gestión de consultas de unidades de flota

--- src/types/menus.ts
+++ src/types/menus.ts
@@ -2,4 +2,5 @@ export enum Menu {
   LIQUIDACION_MATERIALES_ATC = 'ListMaterialesATCFragment',
   LIQUIDACION_MATERIALES_OBRAS = 'LiquidacionMaterialesFragment',
   LIQUIDACION_MATERIALES_OBRAS_ENERGIA = 'LiquidacionMaterialesFragment2',
+  CONSULTA_UNIDADES_FLOTA = 'ConsultaUnidadesFragment',
 }
