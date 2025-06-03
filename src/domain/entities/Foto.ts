e033ea54a16538c64b5978d8d47e0fb75d4f667d
Wendell Smith <wendell18cuenta@gmail.com>
2025-06-03 11:01:02 -0500

    fix: Hacer opcionales los campos isComentario e isSave en ParamsFoto
    - Se han modificado los campos `isComentario` e `isSave` en la interfaz `ParamsFoto` para que sean opcionales, mejorando la flexibilidad en su uso.

--- src/domain/entities/Foto.ts
+++ src/domain/entities/Foto.ts
@@ -6,7 +6,7 @@ export interface Foto {
 export interface ParamsFoto {
   maxFotos: number;
   minFotos: number;
-  isComentario: boolean;
-  isSave: boolean;
+  isComentario?: boolean;
+  isSave?: boolean;
   onSave?: (fotos: Foto[]) => Promise<void>;
 }
