8e3c281ea2a280e091dfe01ad02888730281f197
Wendell Smith <wendell18cuenta@gmail.com>
2025-07-01 09:57:16 -0500

    feat: Agregar soporte para estilos personalizados en CustomKeyboardAvoidingView

--- src/presentation/components/ui/CustomKeyboardAvoidingView.tsx
+++ src/presentation/components/ui/CustomKeyboardAvoidingView.tsx
@@ -1,8 +1,14 @@
-import {KeyboardAvoidingView, Platform} from 'react-native';
+import {
+  KeyboardAvoidingView,
+  Platform,
+  StyleProp,
+  ViewStyle,
+} from 'react-native';
 
 interface Props {
   children: React.ReactNode;
   keyboardVerticalOffset?: number;
+  style?: StyleProp<ViewStyle>;
   behavior?: 'height' | 'position' | 'padding';
 }
 
@@ -10,13 +16,14 @@ const CustomKeyboardAvoidingView = ({
   children,
   keyboardVerticalOffset = 80,
   behavior,
+  style,
 }: Props) => {
   return (
     <KeyboardAvoidingView
       behavior={
         behavior ? behavior : Platform.OS === 'ios' ? 'padding' : 'height'
       }
-      style={{flex: 1}}
+      style={[{flex: 1}, style]}
       keyboardVerticalOffset={
         Platform.OS === 'ios'
           ? keyboardVerticalOffset + 40
