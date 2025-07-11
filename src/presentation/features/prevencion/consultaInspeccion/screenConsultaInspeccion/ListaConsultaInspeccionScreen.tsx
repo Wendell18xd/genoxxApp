8fb8e31279ca5173349e05574d54da01ed6df914
Francesca <francescadelzo14@gmail.com>
2025-07-11 11:59:42 -0500

    feat: Update ListaConsultaInspeccionScreen to use DrawerLayout and display inspection consultation text

--- src/presentation/features/prevencion/consultaInspeccion/screenConsultaInspeccion/ListaConsultaInspeccionScreen.tsx
+++ src/presentation/features/prevencion/consultaInspeccion/screenConsultaInspeccion/ListaConsultaInspeccionScreen.tsx
@@ -1,6 +1,12 @@
+import {Text} from 'react-native-paper';
+import DrawerLayout from '../../../main/layout/DrawerLayout';
 
 const ListaConsultaInspeccionScreen = () => {
-  return <div>ListaConsultaInspeccionScreen</div>;
+  return (
+    <DrawerLayout title="Consulta">
+      <Text>Consulta de Inspección</Text>
+    </DrawerLayout>
+  );
 };
 
 export default ListaConsultaInspeccionScreen;
