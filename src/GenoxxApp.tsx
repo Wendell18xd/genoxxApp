1083e9f7976878f31d5c1aba05e4f66decc48feb
Wendell <wendell18cuenta@gmail.com>
2025-07-17 15:08:39 -0500

    feat: integrate Firebase messaging for push notifications
    - Added Firebase dependencies to package.json for messaging support.
    - Implemented notification permission request in GenoxxApp.
    - Registered device for remote messages and retrieved FCM token.
    - Set up a listener for incoming FCM messages and displayed alerts.
    - Added google-services.json for Firebase configuration.

--- src/GenoxxApp.tsx
+++ src/GenoxxApp.tsx
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {ThemeContextProvider} from './presentation/context/ThemeContext';
-import {StatusBar, useColorScheme} from 'react-native';
+import {Alert, StatusBar, useColorScheme} from 'react-native';
 import AppNavigation from './presentation/navigations/AppNavigation';
 import Toast from 'react-native-toast-message';
 import {useEffect, useState} from 'react';
@@ -11,6 +11,8 @@ import LoadingScreen from './presentation/components/ui/loaders/LoadingScreen';
 import PermissionChecker from './presentation/providers/PermissionChecker';
 import {initDB} from './presentation/services/database/database';
 import {useBackgroundSync} from './presentation/hooks/useBackgroundSync';
+import {PermissionsAndroid} from 'react-native';
+import messaging from '@react-native-firebase/messaging';
 
 const queryClient = new QueryClient();
 
@@ -20,16 +22,55 @@ const GenoxxApp = () => {
   const [loading, setLoading] = useState(true);
   useBackgroundSync();
 
+  const resquestPermissions = async () => {
+    try {
+      const result = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,
+      );
+
+      if (result === PermissionsAndroid.RESULTS.GRANTED) {
+        //todo: request for device token
+        requestToken();
+      } else {
+        Alert.alert(
+          'Permisos de notificaciones denegados',
+          'Para recibir notificaciones, debes permitir el acceso a las notificaciones en la configuración de tu dispositivo.',
+        );
+      }
+    } catch (error) {
+      console.error('Error requesting permissions:', error);
+    }
+  };
+
+  const requestToken = async () => {
+    try {
+      await messaging().registerDeviceForRemoteMessages();
+      const token = await messaging().getToken();
+      console.log(token);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
     initApi().then(() => {
       initDB()
         .then(() => {
           setLoading(false);
+          resquestPermissions();
         })
         .catch(error => {
           console.error('Error initializing database:', error);
         });
     });
+  }, []);
+
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
+
+    return unsubscribe;
   }, []);
 
   if (loading) {
